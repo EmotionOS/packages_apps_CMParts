LineageOS/android_packages_apps_CMParts

CMParts: Use NotificationManager to figure out LED capabilities
As of f/b change I7d627914b058861048071fc15776031c4152157f, there's
a more generic helper method in the notification service to get a
more unified (and extensible) list of what the on-device LEDs can
do. Move away from direct usage of the boolean resources.

Change-Id: Ib9a6f28dc52af722dac168ec219b790ad94d6a19

src/org/cyanogenmod/cmparts/notificationlight/ApplicationLightPreference.java
@@ -18,6 +18,7 @@ package org.cyanogenmod.cmparts.notificationlight;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.ShapeDrawable;
@@ -63,8 +64,8 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
      */
     public ApplicationLightPreference(Context context, AttributeSet attrs) {
         this(context, attrs, DEFAULT_COLOR, DEFAULT_TIME, DEFAULT_TIME,
-                context.getResources().getBoolean(
-                com.android.internal.R.bool.config_ledCanPulse));
+                context.getSystemService(NotificationManager.class)
+                        .doLightsSupport(NotificationManager.LIGHTS_LED_PULSE));
     }
 
     /**
@@ -75,8 +76,9 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
      */
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue) {
-        this(context, attrs, color, onValue, offValue, context.getResources().getBoolean(
-                com.android.internal.R.bool.config_ledCanPulse));
+        this(context, attrs, color, onValue, offValue,
+                context.getSystemService(NotificationManager.class)
+                        .doLightsSupport(NotificationManager.LIGHTS_LED_PULSE));
     }
 
     /**
@@ -121,7 +123,8 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
         TextView tView = (TextView) holder.findViewById(android.R.id.summary);
         tView.setVisibility(View.GONE);
 
-        if (!getContext().getResources().getBoolean(com.android.internal.R.bool.config_multiColorNotificationLed)) {
+        final NotificationManager nm = getContext().getSystemService(NotificationManager.class);
+        if (!nm.doLightsSupport(NotificationManager.LIGHTS_RGB_LED)) {
             mLightColorView.setVisibility(View.GONE);
         }
 

EmotionOS/packages_apps_CMParts

src/org/cyanogenmod/cmparts/notificationlight/ApplicationLightPreference.java
@@ -18,6 +18,7 @@ package org.cyanogenmod.cmparts.notificationlight;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.ShapeDrawable;
@@ -63,8 +64,8 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
      */
     public ApplicationLightPreference(Context context, AttributeSet attrs) {
         this(context, attrs, DEFAULT_COLOR, DEFAULT_TIME, DEFAULT_TIME,
-                context.getResources().getBoolean(
-                com.android.internal.R.bool.config_ledCanPulse));
+                context.getSystemService(NotificationManager.class)
+                        .doLightsSupport(NotificationManager.LIGHTS_PULSATING_LED));
     }
 
     /**
@@ -75,8 +76,9 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
      */
     public ApplicationLightPreference(Context context, AttributeSet attrs,
                                       int color, int onValue, int offValue) {
-        this(context, attrs, color, onValue, offValue, context.getResources().getBoolean(
-                com.android.internal.R.bool.config_ledCanPulse));
+        this(context, attrs, color, onValue, offValue,
+                context.getSystemService(NotificationManager.class)
+                        .doLightsSupport(NotificationManager.LIGHTS_PULSATING_LED));
     }
 
     /**
@@ -121,7 +123,8 @@ public class ApplicationLightPreference extends CustomDialogPreference<LightSett
         TextView tView = (TextView) holder.findViewById(android.R.id.summary);
         tView.setVisibility(View.GONE);
 
-        if (!getContext().getResources().getBoolean(com.android.internal.R.bool.config_multiColorNotificationLed)) {
+        final NotificationManager nm = getContext().getSystemService(NotificationManager.class);
+        if (!nm.doLightsSupport(NotificationManager.LIGHTS_RGB_NOTIFICATION_LED)) {
             mLightColorView.setVisibility(View.GONE);
         }
 