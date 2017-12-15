LineageOS/android_packages_apps_CMParts

CMParts: gestures: Add volume down/up actions
Change-Id: Ibf0037787bd1886952d263ac679cbd1f1ea45a84

src/org/cyanogenmod/cmparts/gestures/TouchscreenGestureSettings.java
@@ -125,6 +125,9 @@ public class TouchscreenGestureSettings extends SettingsPreferenceFragment {
                     return R.drawable.ic_gesture_action_previous_track;
                 case TouchscreenGestureConstants.ACTION_NEXT_TRACK:
                     return R.drawable.ic_gesture_action_next_track;
+                case TouchscreenGestureConstants.ACTION_VOLUME_DOWN:
+                case TouchscreenGestureConstants.ACTION_VOLUME_UP:
+                    return R.drawable.ic_gesture_action_volume;
                 default:
                     // No gesture action
                     return R.drawable.ic_gesture_action_none;

EmotionOS/packages_apps_CMParts

src/org/cyanogenmod/cmparts/gestures/TouchscreenGestureSettings.java
@@ -130,6 +130,10 @@ public class TouchscreenGestureSettings extends SettingsPreferenceFragment {
                     return R.drawable.ic_gesture_action_previous_track;
                 case TouchscreenGestureConstants.ACTION_NEXT_TRACK:
                     return R.drawable.ic_gesture_action_next_track;
+                case TouchscreenGestureConstants.ACTION_VOLUME_DOWN:
+                    return R.drawable.ic_gesture_action_volume_down;
+                case TouchscreenGestureConstants.ACTION_VOLUME_UP:
+                    return R.drawable.ic_gesture_action_volume_up;
                 default:
                     // No gesture action
                     return R.drawable.ic_gesture_action_none;